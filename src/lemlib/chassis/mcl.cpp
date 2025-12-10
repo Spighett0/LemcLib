cdd92f2d17e8bc6e2b88c41295061910787751d2
Spighett0 <ehenchua.au@gmail.com>
2025-12-10 18:08:32 +1100

    Created mcl file
    - created header file and cpp file for mcl
    - defined particles, started function for initialization

---
+++ src/lemlib/chassis/mcl.cpp
@@ -0,0 +1,12 @@
+#include <math.h>
+#include "pros/rtos.hpp"
+#include "lemlib/util.hpp"
+#include "lemlib/chassis/odom.hpp"
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+
+const int PARTICLES_QUANTITY = 200;
+
+void initializeParticles() {
+
+}
